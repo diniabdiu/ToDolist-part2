a81a543277ee809ff13b3ad0ad01753fb7e0e968
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-01 19:28:17 +0200

    todo list styling

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //check off specific todos by clicking
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
     // if ($(this).css('color') === 'rgb(128, 128, 128)') {
     //     $(this).css({
     //         color: 'black',
@@ -15,9 +15,33 @@ $('li').click(function () {
 });
 
 //click on X to delete Todo
-$('span').click(function(e) {
-    $(this).parent().fadeOut(500, function() {
+$('ul').on('click', 'span', function (e) {
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     e.stopPropagation();
+});
+$('input[type=text]').keypress(function (e) {
+    if (e.which === 13) {
+        e.preventDefault();
+
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val('');
+        
+        if(todoText === ''){
+            console.log(this);
+            $(this).attr('placeholder', 'Fill the text');
+            $(this).css({
+                'border-color': 'red'
+            });
+                
+        };
+
+        if(todoText !== '') {
+            //create a new li and add to ul
+            $('ul').append('<li><span><i class="fas fa-trash-alt"></i></span> ' + todoText + '</li>');
+        }
+    }
+
 });
